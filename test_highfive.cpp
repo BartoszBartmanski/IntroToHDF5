6c90b91d407452c42099938f9f6f7612aec1cad4
Bartosz Bartmanski <bartoszbartmanski@gmail.com>
2021-04-14 14:23:50 +0200

    Small changes in test_highfive.cpp - testing objects loaded from an hdf5 file.

--- test_highfive.cpp
+++ test_highfive.cpp
@@ -30,16 +30,45 @@ int main() {
 
 	// Reading in data generated using another package
     H5Easy::File file2("test2_rhdf5.h5", H5Easy::File::ReadOnly);
+    std::cout << std::endl << "A = " << std::endl;
 	auto test = H5Easy::load<std::vector<double>>(file2, "A");
     for (auto& i : test) {
         std::cout << i << std::endl;
     }
 
     auto test2 = H5Easy::load<std::vector<std::vector<double>>>(file2, "B");
+    std::cout << std::endl << "B = " << std::endl;
     for (auto& i : test2) {
         for (auto& j : i) {
             std::cout << j << " ";
         }
         std::cout << std::endl;
     }
+
+    // Testing picking columns from matrix based on a condition
+    std::vector<int> cond = {1, 0};
+
+    std::vector<std::vector<double>> new_mat;
+    for (const auto& row : test2) {
+        std::vector<double> temp;
+        for (unsigned i=0; i < row.size(); i++) {
+            if (cond[i] != 0) {
+                temp.push_back(row[i]);
+            }
+        }
+        new_mat.push_back(temp);
+    }
+
+    std::cout << std::endl << "Subset of matrix:" << std::endl;
+    for (const auto& row : new_mat) {
+        for (const auto& elem : row) {
+            std::cout << elem << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::vector<int> test3 = {0, 1, 1, 1};
+    std::cout << std::endl << "Number of ones in the vector:" << std::endl;
+    std::cout << std::count(test3.begin(), test3.end(), 1) << std::endl;
+    
 } 
